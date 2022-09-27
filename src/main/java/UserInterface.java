92e0ce2ca757c6b87d305e9b982d9612723c764d
Zana0046@stud.kea.dk <90389855+Dannicolak@users.noreply.github.com>
2022-09-27 14:30:24 +0200

    l

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -3,20 +3,19 @@ import java.util.Scanner;
 public class UserInterface {
     private Scanner sc = new Scanner(System.in);
 
+    public void startprgram() {
+        //UserInput
+            System.out.println("""
+                    Welcome To The Adventure Game
+                    Where do you want to go?
+                    7. Look for the description of the room
+                    8. Help for instructions
+                    9. Exit the game
+                                
+                        """);
+            String name = sc.nextLine();
+            int number = sc.nextInt();
 
-    //UserInput
-    public UserInterface (){
-        System.out.println("""
-    Welcome To The Adventure Game
-    Where do you want to go?
-    7. Look for the description of the room
-    8. Help for instructions
-    9. Exit the game
-                
-        """);
-        String name = sc.nextLine();
-        int number = sc.nextInt();
-
-        switch ()
+            switch ()
+        }
     }
-}
