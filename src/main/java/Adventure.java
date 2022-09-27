177d1caf39ebbe88af5e65ca6416c60ac6353e8c
Zana0046@stud.kea.dk <90389855+Dannicolak@users.noreply.github.com>
2022-09-27 14:40:37 +0200

    switch

--- src/main/java/Adventure.java
+++ src/main/java/Adventure.java
@@ -19,34 +19,27 @@ public class Adventure {
         room1.setEast(room2);
         room1.setSouth(room4);
 
-        currentRoom = room2;
         room2.setEast(room3);
         room2.setWest(room1);
 
-        currentRoom = room3;
         room3.setSouth(room6);
         room3.setWest(room2);
 
-        currentRoom = room4;
         room4.setEast(room2);
         room4.setSouth(room4);
 
-        currentRoom = room5;
         room5.setEast(room2);
 
-        currentRoom = room6;
         room6.setSouth(room4);
         room6.setEast(room2);
 
-        currentRoom = room7;
         room7.setSouth(room4);
         room7.setEast(room2);
 
-        currentRoom = room8;
+
         room8.setSouth(room4);
         room8.setEast(room2);
 
-        currentRoom = room9;
         room9.setEast(room2);
         room9.setSouth(room4);
 
