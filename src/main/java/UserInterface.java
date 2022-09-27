b6eaf1a02e37db6d0bfeaafcfa9bcaed0a11852b
Zana0046@stud.kea.dk <90389855+Dannicolak@users.noreply.github.com>
2022-09-27 12:49:57 +0200

    userinput

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -9,6 +9,9 @@ public class UserInterface {
         System.out.println("""
     Welcome To The Adventure Game
     Where do you want to go?
+    7. Look for the description of the room
+    8. Help for instructions
+    9. Exit the game
                 
         """);
     }
