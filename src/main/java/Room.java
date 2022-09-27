614418ca8108afcf51848dffa4f0b74a19581b05
Zana0046@stud.kea.dk <90389855+Dannicolak@users.noreply.github.com>
2022-09-27 13:42:06 +0200

    room

--- src/main/java/Room.java
+++ src/main/java/Room.java
@@ -9,13 +9,37 @@ public class Room {
     public Room (String name, String description){
         this.name = name;
         this.description = description;
-
     }
+
     public void setName(){
         name = name;
 
     }
     public void setDescription(){
         description = description;
+    }
+    public void setNorth (){
+        north = north;
+    }
+    public void setEast (){
+        east = east;
+    }
+    public void setWest (){
+        west = west;
+    }
+    public void setSouth (){
+        south = south;
+    }
+    public Room getNorth() {
+        return north;
+    }
+    public Room getEast (){
+        return east;
+    }
+    public Room getWest (){
+        return west;
+    }
+    public Room getSouth (){
+        return south;
     }
 }
