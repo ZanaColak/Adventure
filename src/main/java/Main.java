5cb520022ac7dec5527942b1726067823757ef68
Zana0046@stud.kea.dk <90389855+Dannicolak@users.noreply.github.com>
2022-09-27 10:10:51 +0200

    m

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-
+        UserInterface userInterface = new UserInterface(); //Metode kald, kaldes på User... klassen
     }
 }
--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+
 public class UserInterface {
+    private Scanner sc = new Scanner(System.in);
+
+
+    //UserInput
+    public UserInterface (){
+        System.out.println("""
+    Welcome To The Adventure Game
+    Where do you want to go?
+                
+        """);
+    }
 }
