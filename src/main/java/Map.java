a69b85fc37e7330d3d4ec2246511bcca40dca987
Zana0046@stud.kea.dk <90389855+Dannicolak@users.noreply.github.com>
2022-10-07 17:21:25 +0200

    Made some changes in map

--- src/main/java/Map.java
+++ src/main/java/Map.java
@@ -1,14 +1,14 @@
 public class Map {
 
-    Room room1 = new Room("Room 1 ", "Room with no distinct features, East & South");
-    Room room2 = new Room("Room 2 ", "Room with no distinct features, East & West");
-    Room room3 = new Room("Room 3 ", "Room with no distinct features, West & South");
-    Room room4 = new Room("Room 4 ", "Room with no distinct features, North & South");
-    Room room5 = new Room("Room 5 ", "Room with no distinct features, South");
-    Room room6 = new Room("Room 6 ", "Room with no distinct features, North & South");
-    Room room7 = new Room("Room 7 ", "Room with no distinct features, North & East");
-    Room room8 = new Room("Room 8 ", "Room with no distinct features, East & West & North");
-    Room room9 = new Room("Room 9 ", "Room with no distinct features, North & West");
+    Room room1 = new Room("Room 1 ", "The room where it all has to begin, there is some great items in this room, only because it is the first room, lucky you!");
+    Room room2 = new Room("Room 2 ", "In this room, there is no food, but there is some great items, that you might need further in this game");
+    Room room3 = new Room("Room 3 ", "In this room, there is a great item and food, but be careful, there is a chance for it to be unhealthy");
+    Room room4 = new Room("Room 4 ", "This is the most exciting room in this adventure! Kali-sticks yeah i know what you are thinking, durum? and shoes?, it is hard to choose one i know, so don't stress, take a break and think");
+    Room room5 = new Room("Room 5 ", "The room where you are stuck, you only have one direction. There is food in this room, but choose wisely if you want to eat it, there is rumors about the berries in this room");
+    Room room6 = new Room("Room 6 ", "Welcome to the most depressing room in this adventure, there is no food, weapons or useful items. I hope that you have something saved in your bag, otherwise i wish you good luck");
+    Room room7 = new Room("Room 7 ", "There is a great great item in this room, i will not say if it is the mushrooms or the item. Choose wisely, it can affect your whole adventure");
+    Room room8 = new Room("Room 8 ", "The room where you have a lot of choices in direction, but not with items. There is only a gun in this room, but it is not (just) a gun, so be careful");
+    Room room9 = new Room("Room 9 ", "Be careful!, there is spiders in this room. Use your brain, maybe you can take it, and use it against an enemy or you can just take the gun. There is also food from McD, (i know it looks great) but look at your healtlevel before you eat it");
 
     public Map() {
         // Room bliver forbundet med en setmetode
@@ -50,19 +50,19 @@ public class Map {
         room9.addItemToRoom("spiders");
 
         //Food Objects
-        room1.addFoodToRoom("apple", 56);
+        room1.addFoodToRoom("apple", 66);
         room3.addFoodToRoom("banana", 76);
-        room4.addFoodToRoom("shawarma", 47);
-        room5.addFoodToRoom("berries with poison", -48);
-        room7.addFoodToRoom("mushrooms woth poison", -79);
+        room4.addFoodToRoom("durum", 40);
+        room5.addFoodToRoom("berries", -48);
+        room7.addFoodToRoom("mushrooms", -79);
         room9.addFoodToRoom("big mac", 32);
 
         //Weapons objects
         room1.addWeaponsToRoom("knife", -50);
-        room3.addWeaponsToRoom("ak-47", -75);
+        room2.addWeaponsToRoom("ak-47", -75);
         room4.addWeaponsToRoom("kali-sticks", -35);
         room5.addWeaponsToRoom("sniper", -100);
-        room7.addWeaponsToRoom("m-13", -85);
+        room8.addWeaponsToRoom("m-13", -85);
         room9.addWeaponsToRoom("desert eagle", -65);
     }
 
